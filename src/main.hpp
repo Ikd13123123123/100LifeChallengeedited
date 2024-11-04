e7c493389f564105af97a46cdf0b33ef61b0edc5
CANTGUESSUSERNAME <ikd62343242343@outlook.com>
2024-11-04 18:12:33 -0500

    Update main.hpp

--- src/main.hpp
+++ src/main.hpp
@@ -3,9 +3,9 @@
 struct Challenge {
 	bool active = false;
 
-	int lives = 100;
-	int practiceRuns = 3;
-	int skips = 3;
+	int lives = 1000;
+	int practiceRuns = 300;
+	int skips = 300;
 	int levels = 0;
 	int coins = 0;
 
